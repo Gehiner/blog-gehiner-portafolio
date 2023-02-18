ee7bf48189c911b7dc75df940aea84481e24f5df
gehiner-windows <gehinerferley@gmail.com>
2023-02-18 00:05:57 -0500

    Header layout

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
-import "../../Styles/main.css"
+import "../../Styles/main.css";
+import "../../Styles/Media-Queries.css";
 const Header=()=>{
     return(
         <header>
             <nav>
-                <div>
-                    <img src="" alt="Logo de la p&aacute;gina" />
+                <div className="logo-container">
+                    <img src="https://i.ibb.co/TYkY1Mw/Copia-de-Logotipo-de-empresa-de-programaci-n-de-sistemas-y-tecnolog-a-copia.webp" alt="Logo de la p&aacute;gina" className="logo"/>
                 </div>
-                <div>
-                    <Link to="">HOME</Link>
-                    <Link to="">SOBRE M&Iacute;</Link>
+                <div className="options-nav">
+                    <a href="">HOME</a>
+                    <a href="">SOBRE M&Iacute;</a>
                 </div>
             </nav>
         </header>
