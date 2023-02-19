8c701e164d97072299a6961e0bd9acecb170758c
gehiner-windows <gehinerferley@gmail.com>
2023-02-18 20:21:07 -0500

    experience page update

--- src/Pages/Experience.js
+++ src/Pages/Experience.js
@@ -19,9 +19,11 @@ const Experience=()=>{
                             <br/>
                             <h3>Royher SAS &#40;dic. 2022 - actualidad  &#41;</h3>
                             <br/>
+                            <br/>
                             <span>Dar solución a las incidencias que se presentan a nivel de software y hardwareo.</span>
                             <br/>
                             <span>Reportar periódicamente el desarrollo de cada solución brindada ante las incidencias presentadas a la gerencia y los clientes externos.</span>
+                            <br/>
                             <br/>
                             <span>Administrar la suite de Microsoft 365 con el objetivo de asegurar el buen funcionamiento de las operaciones administrativas y logísticas.</span>
                         </div>
@@ -38,15 +40,20 @@ const Experience=()=>{
                             <span>Desarrollar funcionalidades de acuerdo con la necesidad de la aplicación en las cuales se implementaron tecnologías de frontend como HTML 5, CSS y Reactjs.
                             </span>
                             <br/>
+                            <br/>
                             <span>Coordinar las tareas diarias de los proyectos para los equipos de frontend y backend.</span>
                             <br/>
+                            <br/>
                             <span>Definir cada interfaz de las aplicaciones web y tiendas virtuales utilizando Figma.   </span>
+                            <br/>
                             <br/>
                             <h3>Logros</h3>
                             <br/>
                             <span>Implementé un CRUD exitosamente mediante un API del Backend (NodeJs) que se conectaba a una base de datos no relacional en Atlas MongoDB.</span>
                             <br/>
+                            <br/>
                             <span>Lideré el proyecto Uke Mochi y Vuela Fácil, que permite a los usuarios contar con una plataforma en la que pueden acceder a productos y servicios, para facilitar su proceso de compra. </span>
+                            <br/>
                             <br/>
                             <span>Desarrollé más de 2 proyectos de diseño de interfaces, utilizando herramientas de programación como HTML, CSS, Javascript y Reactjs.</span>
                         </div>
@@ -62,7 +69,9 @@ const Experience=()=>{
                             <br/>
                             <span>Crear animaciones y videos utilizando  Illustrator, After Effects y Premiere para los streamings de la Parroquia Santísimo Redentor y Seminario San Alfonso.</span>
                             <br/>
+                            <br/>
                             <span>Definir la parrilla de contenido y el guión  de cada producción de  la Parroquia Santísimo Redentor y Seminario San Alfonso.</span>
+                            <br/>
                             <br/>
                             <h3>Logros</h3>
                             <br/>
