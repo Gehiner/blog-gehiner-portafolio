8880ef05a62169bd195bdaf9b92441f7e2910855
gehiner-windows <gehinerferley@gmail.com>
2023-05-22 21:57:05 -0500

    add link Yard Sale

--- src/Pages/Proyects.js
+++ src/Pages/Proyects.js
@@ -14,7 +14,7 @@ const Proyects=()=>{
                     <a href="https://u3405-ukemochi-frontend-app.vercel.app/"> <img src="https://i.ibb.co/vcFxSNM/ukeMochi.webp" alt="UkeMochi" /></a>
                     <a href="https://gehiner.github.io/"> <img src="https://i.ibb.co/dQy7ZWJ/miBlog.webp" alt="Blog-Github" /></a>
                     <a href="https://gitlab.com/05401/vuelafacil"> <img src="https://i.ibb.co/d64Y8rh/Vuela-Facil.webp" alt="Vuela Facil" /></a>
-                    <a href="https://github.com/Gehiner/Yard-Sale-Curso-Practico-Platzi"> <img src="https://i.ibb.co/7Xr5Nwc/YardSale.webp" alt="Yard-Sale" /></a>
+                    <a href="https://gehiner.github.io/Yard-Sale-Curso-Practico-Platzi/"> <img src="https://i.ibb.co/7Xr5Nwc/YardSale.webp" alt="Yard-Sale" /></a>
                     <a href="https://github.com/Gehiner/BataBit-Platzi"> <img src="https://i.ibb.co/yfTtWR6/Batabit.webp" alt=" Batatabit" /></a>
                     <a href="https://github.com/Gehiner/Viking"> <img src="https://i.ibb.co/wSXJmNK/Vikingo.webp" alt="Vikingo" /></a>
                     <a href="https://github.com/Gehiner/GridPractica"> <img src="https://i.ibb.co/hgTLGYm/Reto-Platzi.webp" alt="Proyecto-Plazti" /></a>
